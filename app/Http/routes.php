9fbfc26b46d732e6fb2dbcfeda4cb975feb546fc
Edgar Zhukovskiy <edzhukovskiy@gmail.com>
2016-04-18 17:03:48 +0400

    Aded route for PriorityController

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,4 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('todo','TodoController');
+Route::resource('todo', 'TodoController');
+Route::resource('priority', 'PriorityController');
